f19a5db6dcce929b3114a290f26a95f7869099e1
Bing Wang <bwang01@uchicago.edu>
2020-10-24 16:25:35 +0900

    adds lint to front end

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  plugins: [
+    '@typescript-eslint',
+    'react-hooks',
+  ],
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    },
+    project: './tsconfig.json',
+  },
+  settings: {
+    react: {
+      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  },
+  extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin,
+    "react-app",
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
